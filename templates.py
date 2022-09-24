4dcec44410f61c01572c82777e8dec32e588ab3f
Steve Winslow <steve@swinslow.net>
2022-09-24 11:24:34 -0400

    Add exceptions and writing to file
    Signed-off-by: Steve Winslow <steve@swinslow.net>

--- templates.py
+++ templates.py
@@ -12,13 +12,36 @@ def makeTemplates(ldbLicenses):
     )
 
     licTemplate = env.get_template("license.xml")
+    excTemplate = env.get_template("exception.xml")
 
     # FIXME === testing ===
     lic = ldbLicenses.get("3com-microcode", None)
-    renderLicense(licTemplate, lic)
-    print(lic._text)
+    renderLicense(licTemplate, excTemplate, lic)
     return True
 
-def renderLicense(licTemplate, lic):
-   print(licTemplate.render(lic=lic, listVersionAdded=LISTVERSIONADDED))
+def renderLicense(licTemplate, excTemplate, lic):
+    if lic._is_exception:
+        tmpl = excTemplate
+        xmlFilename = os.path.join("xml", "exceptions", lic._spdx_chosen_id+".xml")
+    else:
+        tmpl = licTemplate
+        xmlFilename = os.path.join("xml", lic._spdx_chosen_id+".xml")
+
+    testTextFilename = os.path.join("text", lic._spdx_chosen_id+".txt")
+
+    try:
+        with open(xmlFilename, "w") as f:
+            f.write(tmpl.render(lic=lic, listVersionAdded=LISTVERSIONADDED))
+    except OSError as e:
+        print("Error writing {xmlFilename}: {str(e)}")
+        return False
+
+    try:
+        with open(testTextFilename, "w") as f:
+            f.write(lic._text)
+    except OSError as e:
+        print("Error writing {testTextFilename}: {str(e)}")
+        return False
+
+    return True
 
